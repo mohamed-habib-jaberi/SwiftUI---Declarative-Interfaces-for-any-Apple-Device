8bb012df52dd553ebd1019542f9c0da976a6b5e2
mohamed  habib <jaberi.mohamedhabib@gmail.com>
2022-08-06 21:00:21 +0200

    initial commit

---
+++ Hiking/Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by mohamed  habib on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

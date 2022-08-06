042be6b0597680de6989a164f2e42e0b282d992d
mohamed  habib <jaberi.mohamedhabib@gmail.com>
2022-08-06 22:28:21 +0200

    Adding Tap and Zooming Animation

--- Hiking/Hiking/HikeDetail.swift
+++ Hiking/Hiking/HikeDetail.swift
@@ -10,16 +10,24 @@ import SwiftUI
 struct HikeDetail: View {
     
     let hike: Hike
+    @State private var zoomed: Bool = false
     
     var body: some View {
         VStack{
             Image(hike.imageURL)
                 .resizable()
-                .aspectRatio(contentMode: .fit)
+                .aspectRatio(contentMode: self.zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation{
+                        self.zoomed.toggle()
+                    }
+                }
+            
             Text(hike.name)
             Text(String(format: "%.2f", hike.miles))
             
         }
+        .navigationBarTitle(hike.name, displayMode: .inline)
     }
 }
 

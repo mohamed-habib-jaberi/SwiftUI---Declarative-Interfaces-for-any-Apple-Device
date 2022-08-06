d5c8f8c2a942d68fa65059c4533a72c63b671e14
mohamed  habib <jaberi.mohamedhabib@gmail.com>
2022-08-06 21:54:01 +0200

    create hike list with navigation

--- Hiking/Hiking/ContentView.swift
+++ Hiking/Hiking/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let hikes = Hike.all()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            List(self.hikes, id: \.name) { hike in
+                NavigationLink(destination: Text(hike.name)) {
+                    HikeCell(hike: hike)
+                }
+            }
+            
+            .navigationBarTitle("Hikings")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
+    }
+}
+
+struct HikeCell: View {
+    
+    let hike: Hike
+    
+    var body: some View {
+        HStack{
+            Image(hike.imageURL)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(16)
+            
+            VStack(alignment: .leading) {
+                Text(hike.name)
+                Text(String(format: "%.2f", hike.miles))
+            }
+        }
     }
 }

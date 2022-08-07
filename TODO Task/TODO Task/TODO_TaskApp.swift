daec4be53824e5225fa051b9b18f2274041e511d
mohamed  habib <jaberi.mohamedhabib@gmail.com>
2022-08-07 20:08:55 +0200

    Adding Items to List Maintained by State

---
+++ TODO Task/TODO Task/TODO_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO_TaskApp.swift
+//  TODO Task
+//
+//  Created by mohamed  habib on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TODO_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

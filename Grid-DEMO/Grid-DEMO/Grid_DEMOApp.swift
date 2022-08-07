04d3ac716b0cb2de38cfd8180c970be72fdabb38
mohamed  habib <jaberi.mohamedhabib@gmail.com>
2022-08-07 18:01:53 +0200

    add grid demo

---
+++ Grid-DEMO/Grid-DEMO/Grid_DEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_DEMOApp.swift
+//  Grid-DEMO
+//
+//  Created by mohamed  habib on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Grid_DEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

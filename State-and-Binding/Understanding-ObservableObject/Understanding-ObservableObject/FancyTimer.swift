3d1f5ae0e2bb39aea5e30e70ddab66bec74e570e
mohamed  habib <jaberi.mohamedhabib@gmail.com>
2022-12-20 14:17:47 +0100

    understanding ObservableObject

---
+++ State-and-Binding/Understanding-ObservableObject/Understanding-ObservableObject/FancyTimer.swift
@@ -0,0 +1,23 @@
+//
+//  FancyTimer.swift
+//  Understanding-ObservableObject
+//
+
+
+import Foundation
+import SwiftUI
+import Combine
+
+class FancyTimer: ObservableObject {
+    
+    @Published var value: Int = 0
+    
+    init() {
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            self.value += 1
+        }
+        
+    }
+    
+}

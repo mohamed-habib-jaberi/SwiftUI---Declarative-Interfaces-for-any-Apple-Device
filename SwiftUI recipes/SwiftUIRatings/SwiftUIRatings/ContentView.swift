034feb3c73f8e96a9de103b150ef683174e016f0
mohamed  habib <jaberi.mohamedhabib@gmail.com>
2022-12-25 13:44:45 +0100

    Building rating view in SwiftUI

---
+++ SwiftUI recipes/SwiftUIRatings/SwiftUIRatings/ContentView.swift
@@ -0,0 +1,21 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var rating: Int?
+    
+    var body: some View {
+        VStack {
+            RatingView(rating: $rating)
+             Text(rating != nil ? "You rating: \(rating!)" : "")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
